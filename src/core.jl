e595d6820166bc0cfb8ac3721ac1cf879a36d795
Leo <lpdiaz@student.unimelb.edu.au>
2021-03-10 15:42:31 +1100

    moving traits to core

--- src/core.jl
+++ src/core.jl
@@ -126,3 +126,30 @@ ChemicalHyperGraph(hes::AbstractVector{ChemicalHyperEdge{T}}) where {T} = Chemic
 (::Type{ChemicalHyperGraph{T}})(hes::AbstractVector{ChemicalHyperEdge{T}}) where {T} = ChemicalHyperGraph(hes)
 ChemicalHyperGraph{T}() where {T} = ChemicalHyperGraph{T}([], [])
 ChemicalHyperGraph() = ChemicalHyperGraph{Any}([], [])
+
+# build unions of abstract types for easier dispatch
+const AbstractStructs = Union{AbstractHyperGraph, AbstractHyperEdge}
+
+# fundamental traits
+@traitdef IsOriented{hg <: AbstractStructs}
+@traitdef IsWeighted{hg <: AbstractStructs}
+
+# functions and default behaviour: orientation
+@traitimpl IsOriented{T} <- isoriented(T)
+isoriented(::Type{T}) where {T} = false
+isoriented(X::T) where {T} = isoriented(T)
+
+# functions and default behaviour: weight
+@traitimpl IsWeighted{T} <- isweighted(T)
+isweighted(::Type{T}) where {T} = false
+isweighted(X::T) where {T} = isweighted(T)
+
+# build unions of types for easier dispatch
+const OrientedStructs = Union{ChemicalHyperGraph, ChemicalHyperEdge}
+const WeightedStructs = Union{ChemicalHyperGraph, ChemicalHyperEdge}
+
+# implementing traits on types
+@traitimpl IsOriented{OrientedStructs}
+isoriented(::Type{T}) where {T<:OrientedStructs} = true
+@traitimpl IsWeighted{WeightedStructs}
+isweighted(::Type{T}) where {T<:WeightedStructs} = true
