8562b1dae70d03dc77b22520f581413320d1d422
Leo <lpdiaz@student.unimelb.edu.au>
2021-03-10 13:23:59 +1100

    set up traits

--- src/HyperGraphs.jl
+++ src/HyperGraphs.jl
@@ -1,12 +1,20 @@
 module HyperGraphs
 
+using SimpleTraits.SimpleTraits
+
 include("core.jl")
+include("traits.jl")
 
 export
 
 # core: types
 AbstractHyperGraph, AbstractHyperEdge,
 HyperEdge, HyperGraph,
-SpeciesSet, ChemicalHyperEdge, ChemicalHyperGraph
+SpeciesSet, ChemicalHyperEdge, ChemicalHyperGraph,
+
+# traits
+OrientedStructs, WeightedStructs, # constants holding type unions
+IsOriented, IsWeighted, # traits
+isoriented, isweighted # trait functions
 
 end # module
