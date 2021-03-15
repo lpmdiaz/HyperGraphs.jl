7e2ace707c61294fcece49bdaaea839c4ae4b9b0
lpmdiaz <lpdiaz@student.unimelb.edu.au>
2021-03-15 13:18:34 +1100

    more core tests & fixed core functions

--- test/core.jl
+++ test/core.jl
@@ -15,6 +15,8 @@ he = HyperEdge(vertices_he)
 vertices_hes = [[1, 2, 3], [2, 3, 4]]
 hes = [HyperEdge(vertices_hes[1]), HyperEdge(vertices_hes[2])]
 @test vertices(hes) == vertices_hes
+@test eltype(he) == Int64
+@test HyperEdge([1, 2, 3]) == he
 
 # hypergraphs: constructors
 HyperGraph()
@@ -22,19 +24,25 @@ HyperGraph{Int8}()
 HyperGraph(HyperEdge([1, :∅]))
 HyperGraph([HyperEdge([1, 1]), HyperEdge([2, 2])])
 HyperGraph(HyperEdge([1, 1]))
+@test HyperGraph([1], HyperEdge{Int}[]) == HyperGraph{Int}([1], HyperEdge[])
+@test HyperGraph(1, HyperEdge([1, 1])) == HyperGraph([1], HyperEdge([1, 1])) == HyperGraph(1, [HyperEdge([1, 1])]) == HyperGraph([1], [HyperEdge([1, 1])])
+@test HyperGraph(hes) == HyperGraph(unique(vcat(vertices_hes...)), hes)
 
 # hypergraphs: functions
 @test vertices(HyperGraph(HyperEdge([1, :∅]))) == [1, :∅]
 hg = HyperGraph(hes)
-@test vertices(hg) == unique(vcat(vertices_hes...))
+@test vertices(hg) == unique(vcat(vertices_hes...)) == [1, 2, 3, 4]
 @test vertices(hyperedges(hg)) == vertices_hes
 @test hyperedges(hg) == hes
+@test eltype(hg) == eltype(hyperedges(hg)[1])
 
 ## chemical hyperedges and chemical hypergraphs ##
 
 # incidence: constructors
 SpeciesSet{Symbol}()
 SpeciesSet()
+@test SpeciesSet([1]) == SpeciesSet(1)
+@test SpeciesSet(["X"]) == SpeciesSet("X") == SpeciesSet("X", 1) == SpeciesSet(["X"], [1])
 
 # incidence: functions
 s = SpeciesSet(:∅)
@@ -42,6 +50,9 @@ s = SpeciesSet(:∅)
 @test multiplicities(s) == [1]
 @test multiplicities(SpeciesSet([1, 2, 3], [1, 1, 2])) == [1, 1, 2]
 @test_throws MethodError SpeciesSet(["X"], [1.2]) # stoichiometries are defined on integers
+@test species(s) == [:∅]
+@test stoich(s) == [1]
+@test length(s) == 1
 
 # chemical hyperedges: constructors
 ChemicalHyperEdge{String}()
@@ -49,6 +60,7 @@ ChemicalHyperEdge()
 ChemicalHyperEdge(["X", "Y"], ["Z"], 2)
 ChemicalHyperEdge(["X", "Y"], ["Z"], [[[2]]]) # is this undesirable?
 ChemicalHyperEdge(["X"], ["Y"])
+@test ChemicalHyperEdge(["X"], ["Y"], 1) == ChemicalHyperEdge(["X"], ["Y"]) # rate defaults to 1
 
 # chemical hyperedges: functions
 reactant = SpeciesSet(["X"])
@@ -66,6 +78,9 @@ ches = [ChemicalHyperEdge(src, tgt) for (src, tgt) in zip(srcs, tgts)]
 @test src(ches) == srcs
 @test tgt(ches) == tgts
 @test vertices(ches) == [[1, 1, 2, 3], [4, 2, 3, 4]]
+@test eltype(reactant) == eltype(che) == String
+@test src_stoich(che) == inputs_stoich(che) == [1]
+@test tgt_stoich(che) == outputs_stoich(che) == [2]
 
 # chemical hypergraphs: constructors
 ChemicalHyperGraph{String}()
@@ -76,3 +91,5 @@ vertices_chg = [1, 2, 3, 4]
 chg = ChemicalHyperGraph(vertices_chg, ches)
 @test vertices(chg) == vertices_chg
 hyperedges(chg)
+@test ChemicalHyperGraph(ches) == chg
+@test eltype(chg) == eltype(hyperedges(chg)[1])
